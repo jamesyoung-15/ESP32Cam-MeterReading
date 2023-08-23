ee4ed1524aef071cd3bea98917f80badf6ee6519
jamesyoung <jamesyoung3931@gmail.com>
2023-08-23 14:06:17 +0800

    add secrets config

---
+++ MeterReadingProject/ESP32-Cam-Server/src/secrets_example.h
@@ -0,0 +1,10 @@
+#ifndef MY_SECRETS_H
+#define MY_SECRETS_H
+
+// ===========================
+// Enter your WiFi credentials
+// ===========================
+const char* ssid = "******";
+const char* password = "******";
+
+#endif
